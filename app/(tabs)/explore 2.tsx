6c5994832c9ea27b5d081e9f278ffda48c806ff5
cpark3918 <cindy_park@bcit.ca>
2025-05-10 18:59:42 +0900

    fixed version / JSX issue from main branch

---
+++ app/(tabs)/explore 2.tsx
@@ -0,0 +1,86 @@
+// import { Image } from "expo-image";
+// import { Platform, StyleSheet } from "react-native";
+
+// import { Collapsible } from "@/components/Collapsible";
+// import { ExternalLink } from "@/components/ExternalLink";
+// import ParallaxScrollView from "@/components/ParallaxScrollView";
+// import { ThemedText } from "@/components/ThemedText";
+// import { ThemedView } from "@/components/ThemedView";
+
+// export default function TabTwoScreen() {
+//   return (
+//     <ParallaxScrollView
+//       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+//     >
+//       <ThemedView style={styles.titleContainer}></ThemedView>
+//       <ThemedText>
+//         This app includes example code to help you get started.
+//       </ThemedText>
+//       <Collapsible title="File-based routing">
+//         <ThemedText>This app has two screens: </ThemedText>
+//         <ThemedText>The layout file in sets up the tab navigator.</ThemedText>
+//         <ExternalLink href="https://docs.expo.dev/router/introduction"></ExternalLink>
+//       </Collapsible>
+//       <Collapsible title="Android, iOS, and web support">
+//         <ThemedText>
+//           You can open this project on Android, iOS, and the web. To open the
+//           web version, press in the terminal running this project.
+//         </ThemedText>
+//       </Collapsible>
+//       <Collapsible title="Images">
+//         <ThemedText>
+//           For static images, you can use the suffixes to provide files for
+//           different screen densities
+//         </ThemedText>
+//         <Image
+//           source={require("@/assets/images/react-logo.png")}
+//           style={{ alignSelf: "center" }}
+//         />
+//         <ExternalLink href="https://reactnative.dev/docs/images"></ExternalLink>
+//       </Collapsible>
+//       <Collapsible title="Custom fonts">
+//         <ThemedText>
+//           Open to see how to load{" "}
+//           <ThemedText style={{ fontFamily: "SpaceMono" }}>
+//             custom fonts such as this one.
+//           </ThemedText>
+//         </ThemedText>
+//         <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font"></ExternalLink>
+//       </Collapsible>
+//       <Collapsible title="Light and dark mode components">
+//         <ThemedText>
+//           This template has light and dark mode support. The hook lets you
+//           inspect what the user&apos;s current color scheme is, and so you can
+//           adjust UI colors accordingly.
+//         </ThemedText>
+//         <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/"></ExternalLink>
+//       </Collapsible>
+//       <Collapsible title="Animations">
+//         <ThemedText>
+//           This template includes an example of an animated component. The{" "}
+//           component uses the powerful library to create a waving hand animation.
+//         </ThemedText>
+//         {Platform.select({
+//           ios: (
+//             <ThemedText>
+//               The component provides a parallax effect for the header image.
+//             </ThemedText>
+//           ),
+//         })}
+//       </Collapsible>
+//     </ParallaxScrollView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   headerImage: {
+//     color: "#808080",
+//     bottom: -90,
+//     left: -35,
+//     position: "absolute",
+//   },
+//   titleContainer: {
+//     flexDirection: "row",
+//     gap: 8,
+//   },
+// });
