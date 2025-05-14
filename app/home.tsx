be56c58ae483c880a78ac55f66e002a731907e16
--unset <cindypark31@gmail.com>
2025-05-14 23:13:15 +0900

    saved active session in redux

--- app/home.tsx
+++ app/home.tsx
@@ -289,7 +289,7 @@ export default function HomeScreen() {
           setNumResponds(numWalkLogs);
           const walkLogs = await getWalkLogs(userData?.userId);
           if (walkLogs?.length > 0) {
-            const todaysLogs = walkLogs.find(
+            const todaysLogs = walkLogs.filter(
               (log: any) =>
                 log.walkedAt.slice(0, 10) === todaysDate.slice(0, 10)
             );
@@ -468,58 +468,57 @@ export default function HomeScreen() {
                 </ThemedText>
               </View>
               <ScrollView style={styles.listContainer}>
-                {walkLogs?.length !== 0 &&
-                  walkLogs?.map((log: WalkLog) => {
-                    const { id, name } = log.theme;
-                    return (
-                      <>
-                        <View style={{ paddingBottom: 15 }}>
-                          <View style={styles.listItems}>
-                            <ThemeIcon themeId={id} />
-                            <ThemedText>
-                              {name} - {log.distance}km
-                            </ThemedText>
-                          </View>
-                          {log.respondedSignals?.length > 0 &&
-                            log.respondedSignals.map((item, key) => {
-                              const { title, description } = item;
-                              const category = signalTypes.find(
-                                (sig) => sig.id === item.categoryId
-                              );
-                              return (
-                                <View style={{ marginLeft: 50 }}>
-                                  <View style={styles.listItems}>
-                                    <SignalIcon
-                                      key={key}
-                                      signal={category}
-                                      size={25}
-                                      imgSize={18}
-                                    />
-                                    <ThemedText
-                                      style={{
-                                        color: colors.darkGray.main,
-                                        fontSize: 12,
-                                      }}
-                                    >
-                                      {title}
-                                    </ThemedText>
-                                  </View>
+                {walkLogs?.map((log: WalkLog) => {
+                  const { id, name } = log.theme;
+                  return (
+                    <>
+                      <View style={{ paddingBottom: 15 }}>
+                        <View style={styles.listItems}>
+                          <ThemeIcon themeId={id} />
+                          <ThemedText>
+                            {name} - {log.distance}km
+                          </ThemedText>
+                        </View>
+                        {log.respondedSignals?.length > 0 &&
+                          log.respondedSignals.map((item, key) => {
+                            const { title, description } = item;
+                            const category = signalTypes.find(
+                              (sig) => sig.id === item.categoryId
+                            );
+                            return (
+                              <View style={{ marginLeft: 50 }}>
+                                <View style={styles.listItems}>
+                                  <SignalIcon
+                                    key={key}
+                                    signal={category}
+                                    size={25}
+                                    imgSize={18}
+                                  />
                                   <ThemedText
                                     style={{
-                                      color: colors.text.gray,
-                                      fontSize: 10,
-                                      marginLeft: 35,
+                                      color: colors.darkGray.main,
+                                      fontSize: 12,
                                     }}
                                   >
-                                    {description}
+                                    {title}
                                   </ThemedText>
                                 </View>
-                              );
-                            })}
-                        </View>
-                      </>
-                    );
-                  })}
+                                <ThemedText
+                                  style={{
+                                    color: colors.text.gray,
+                                    fontSize: 10,
+                                    marginLeft: 35,
+                                  }}
+                                >
+                                  {description}
+                                </ThemedText>
+                              </View>
+                            );
+                          })}
+                      </View>
+                    </>
+                  );
+                })}
               </ScrollView>
             </>
           )}
--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import routeReducer from "./slices/routeSlice";
 import walkReducer from "./slices/walkSlice";
+import sessionReducer from "./slices/sessionSlice";
 
 export const store = configureStore({
   reducer: {
     route: routeReducer,
     walk: walkReducer,
+    session: sessionReducer,
   },
 });
 
